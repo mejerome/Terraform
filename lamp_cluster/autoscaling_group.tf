194484c7ce4b6da84e0ac069079ef5fe2ccc4f28
Jerome Tabiri <jtabiri@gmail.com>
2021-04-20 09:15:41 +0100

    working ansible config of autoscaling nodes

--- lamp_cluster/autoscaling_group.tf
+++ lamp_cluster/autoscaling_group.tf
@@ -1,81 +1,77 @@
 
-resource "aws_launch_configuration" "web" {
-  name_prefix   = "web-"
-  image_id      = "ami-096cb92bb3580c759" # eu-west-2
-  instance_type = "t3.micro"
-  key_name      = "jt-london"
+# resource "aws_launch_configuration" "web" {
+#   name_prefix   = "web-"
+#   image_id      = data.aws_ami.lamp-web-ami.id # eu-west-2
+#   instance_type = "t3.micro"
+#   key_name      = "jt-london"
 
-  # user_data       = file("install_apache.sh")
-  security_groups = [aws_security_group.allow_ports.id]
+#   # user_data       = file("install_apache.sh")
+#   security_groups = [aws_security_group.allow_ports.id]
 
-  lifecycle {
-    create_before_destroy = true
-  }
+#   lifecycle {
+#     create_before_destroy = true
+#   }
 
-}
+# }
 
-resource "aws_autoscaling_group" "web" {
-  name                 = "terraform-asg-lamp"
-  launch_configuration = aws_launch_configuration.web.name
-  min_size             = 1
-  desired_capacity     = 2
-  max_size             = 4
+# resource "aws_autoscaling_group" "web" {
+#   name                 = "terraform-asg-lamp"
+#   launch_configuration = aws_launch_configuration.web.name
+#   min_size             = 1
+#   desired_capacity     = 1
+#   max_size             = 4
 
-  health_check_type = "ELB"
-  load_balancers    = [aws_elb.web.id]
+#   health_check_type = "ELB"
+#   load_balancers    = [aws_elb.web.id]
 
-  enabled_metrics = [
-    "GroupMinSize",
-    "GroupMaxSize",
-    "GroupDesiredCapacity",
-    "GroupInServiceInstances",
-    "GroupTotalInstances"
-  ]
+#   enabled_metrics = [
+#     "GroupMinSize",
+#     "GroupMaxSize",
+#     "GroupDesiredCapacity",
+#     "GroupInServiceInstances",
+#     "GroupTotalInstances"
+#   ]
 
-  metrics_granularity = "1Minute"
+#   metrics_granularity = "1Minute"
 
-  vpc_zone_identifier = [aws_subnet.public_sub.id]
+#   vpc_zone_identifier = [aws_subnet.public_sub.id]
 
-  lifecycle {
-    create_before_destroy = true
-  }
+#   lifecycle {
+#     create_before_destroy = true
+#   }
 
-  provisioner "local-exec" {
-    command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i '${self.public_dns},' --private-key '${var.private_key}' apache-install.yml"
-  }
+#   tags = [{
+#     Name = "web-group"
+#   }]
 
-  tags = [{
-    Name = "web-group"
-  }]
+# }
 
-}
+# resource "aws_elb" "web" {
+#   name = "lamp-terraform-elb"
 
-resource "aws_elb" "web" {
-  name = "lamp-terraform-elb"
+#   listener {
+#     instance_port     = 80
+#     instance_protocol = "http"
+#     lb_port           = "80"
+#     lb_protocol       = "http"
+#   }
 
-  listener {
-    instance_port     = 80
-    instance_protocol = "http"
-    lb_port           = "80"
-    lb_protocol       = "http"
-  }
+#   health_check {
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#     timeout             = 3
+#     target              = "HTTP:80/"
+#     interval            = 30
+#   }
 
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:80/"
-    interval            = 30
-  }
+#   subnets                     = [aws_subnet.public_sub.id]
+#   cross_zone_load_balancing   = true
+#   idle_timeout                = 400
+#   connection_draining         = true
+#   connection_draining_timeout = 400
+#   security_groups             = [aws_security_group.allow_ports.id]
 
-  subnets                     = [aws_subnet.public_sub.id]
-  cross_zone_load_balancing   = true
-  idle_timeout                = 400
-  connection_draining         = true
-  connection_draining_timeout = 400
-  security_groups             = [aws_security_group.allow_ports.id]
-
-  tags = {
-    Name = "lamp-terraform-elb"
-  }
-}
+#   tags = {
+#     Name = "lamp-terraform-elb"
+#   }
+# }
--- lamp_cluster/main.tf
+++ lamp_cluster/main.tf
@@ -11,23 +11,36 @@ terraform {
   }
 }
 
+data "aws_ami" "lamp-web-ami" {
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  filter {
+    name   = "tag:Name"
+    values = ["packer-ansible"]
+  }
+  owners      = ["self"]
+  most_recent = true
+}
+
 variable "private_key" {
   type    = string
   default = "../../../jt-london.pem"
 }
 
-# resource "aws_instance" "test" {
-#   ami             = "ami-096cb92bb3580c759" # eu-west-2
-#   instance_type   = "t2.micro"
-#   subnet_id       = aws_subnet.public_sub.id
-#   security_groups = [aws_security_group.allow_ports.id]
-#   key_name        = "jt-london"
+resource "aws_instance" "test" {
+  ami             = "ami-06178cf087598769c" # eu-west-2
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.public_sub.id
+  security_groups = [aws_security_group.allow_ports.id]
+  key_name        = "jt-london"
 
-#   tags = {
-#     Name = "network-tester"
-#   }
+  tags = {
+    Name = "network-tester"
+  }
 
-#   provisioner "local-exec" {
-#     command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i '${self.public_dns},' --private-key '${var.private_key}' apache-install.yml"
-#   }
-# }
+  # provisioner "local-exec" {
+  #   command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu -i '${self.public_dns},' --private-key '${var.private_key}' apache-install.yml"
+  # }
+}
